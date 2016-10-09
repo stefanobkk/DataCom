815a83ebd11421f2492576f0fc1e2a9cfc67c6c3
stefanocassio <stefano.cassiobkk@gmail.com>
2016-10-09 23:31:04 +0700

    Progress status complete

--- project1.py
+++ project1.py
@@ -3,49 +3,55 @@ import requests
 import sys
 import urllib2
 import time
-
-def make_request(url):
-	NL = "\r\n"
-	print "new requestisssss........"
-	return ("GET /HTTP/1.1" +NL +
-		"Host:{urls}" +NL +
-		"Connection: keep-alive" + NL +
-		"Upgrade-Insecure-Requests: 1"+ NL +
-		NL).format(urls = url)
+import socket as skt 
+from time import sleep
+import sys
 
 
+def speedTest(start, size, file_name):
+	total = time.time() - start
+	print "Reading took %.2f seconds, transfer rate %.2f KBPS" %(total, (size/1024.0)/ total)
 
-def speedTest(start, size, text, file_name):
-	total = time.clock() - start
-	print "%s reading took %d seconds, transfer rate %.2f KBPS" %(text, total, (size/1024.0)/ total)
-	print "Page/File size: %sKB" %(size)
-	print "Saving file to: %s" %(file_name)
+def progess(bytes_so_far, chunk_size, total_size):
+	percent = float(bytes_so_far)/total_size
+	percent = round(percent*100, 2)
+	sys.stdout.write("Downloaded %d of %d bytes (%.2f%%)\r" %(bytes_so_far, total_size, percent))
+
+	if bytes_so_far >= total_size:
+		sys.stdout.write('\n')
 
 def Main():
-	#url = "http://pantip.com"
 	user_filename = sys.argv[0]
 	action = sys.argv[1]
 	new_file_name = sys.argv[2]
 	website_url = sys.argv[3]
 
-	aa = make_request(website_url)
-	print aa
-
-
-	#Chuncked
-	f = urllib2.urlopen(website_url)
-	start_time = time.clock()
+	chunk_size = 1024
 	data = []
-	chunk = f.read(1024)
+	start_time = time.time()
+
+	response = requests.get(website_url, stream=True)
+	total_length = response.headers.get('content-length')
+	total_length_int = float(total_length)
+	print "Total download size: %.2fKB" %(total_length_int/chunk_size)
+
+	f = urllib2.urlopen(website_url)
+	chunk = f.read(chunk_size)
+	bytes_so_far = 0
 	while chunk:
 		data.append(chunk)
-		chunk = f.read(1024)
-		print chunk
+		chunk = f.read(chunk_size)
+		bytes_so_far += len(chunk)
 		if not chunk:
 			break 
-	speedTest(start_time, len(data), 'Chuncked', new_file_name)
+		if bytes_so_far<= total_length_int:
+			progess(bytes_so_far, chunk_size, total_length_int)
+
+	speedTest(start_time, total_length_int, new_file_name)
 	f.close() 
 
 
 if __name__ == '__main__':
 	Main()
+
+
