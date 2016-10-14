4db6c0706f1983845bfe67b18360aa680a8bb6c5
stefanocassio <stefano.cassiobkk@gmail.com>
2016-10-14 20:21:21 +0700

    Resume works w/o condition check

--- project1_1.py
+++ project1_1.py
@@ -14,16 +14,30 @@ def make_request(url, path):
 
 def make_resume_req(path,host,obj,_byte):
 	NL = '\r\n'
-	return ("GET {o}{p} HTTP/1.1" 
+	return ("GET {p} HTTP/1.1" 
 		+ NL +"Host: {h}" 
 		+ NL +"Connection: close"
 		+ NL +"Range: {b}"+NL+NL).format(p=path,o=obj,h=host,b=_byte)
 
+def speedTest(start, size, file_name):
+	total = time.time() - start
+	print "Reading took %.2f seconds, transfer rate %.2f KBPS" %(total, (size/1024.0)/ total)
+
+def progess(bytes_so_far, chunk_size, total_size, start, size, file_name):
+	total = time.time() - start
+	percent = float(bytes_so_far)/total_size
+	percent = round(percent*100, 2)
+	aa = int((size/chunk_size)/total)
+	sys.stdout.write("Downloaded %d of %d bytes (%.2f%%) \r" %(bytes_so_far, total_size, percent))
+	if bytes_so_far >= total_size:
+		sys.stdout.write('\n')
+
+
 
 def Main():
 
-	#downloads()
-	resume()
+	downloads()
+	#resume()
 
 
 def downloads():
@@ -103,18 +117,8 @@ def downloads():
 			Date_Modified +=x
 			#print Date_Modified.split[":"]
 
-
 		NL = '\r\n'
 		temp_file.write(Etag + NL + Content_length + NL+  byte_recieved_str + NL+ Date_Modified)
-
-
-	#make_resume_req(URL, '/', byte_recieved)
-	#print make_resume_req
-
-
-	#Find size of the temp file and check if there is anythng inside it
-	#	if there is something inside then we enter resume()
-	#	else enter new Download()	
 
 
 	print byte_recieved, " Bytes received"
@@ -139,31 +143,31 @@ def resume():
 		if x =='\r':
 			break
 		ETag += x
-	ETag = ETag.split(': ')
+	ETag_split = ETag.split(': ')
+
 
 	Content_length = ""
 	for x in temp_file[temp_file.find('Content-Length:'):]:
 		if x =='\r':
 			break
 		Content_length +=x
-	Content_length = Content_length.split(': ')
+	Content_length_split = Content_length.split(': ')
 
 	Date_Modified = ""
 	for x in temp_file[temp_file.find('Last-Modified:'):]:
 		if x =='\r':
 			break
 		Date_Modified +=x
-	Date_Modified = Date_Modified.split(': ')
+	Date_Modified_split = Date_Modified.split(': ')
 
 	byte_recieved =""
 	for x in temp_file[temp_file.find('Byte-recieved:'):]:
 		if x =='\r':
 			break
 		byte_recieved +=x
-	byte_recieved = byte_recieved.split(': ')
+	byte_recieved_split = byte_recieved.split(': ')
 
-
-	request_2 = make_resume_req(PATH, URL, '/', "bytes="+str(byte_recieved[1])+"-")
+	request_2 = make_resume_req(PATH, URL, '/', "bytes="+str(byte_recieved_split[1])+"-")
 	clientSocket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
 	if parseStr.port == None:
 		url3 = URL.find("/")
@@ -180,13 +184,12 @@ def resume():
 	while "\r\n\r\n" not in header2:
 		result2 = clientSocket.recv(1)
 		header2 += result2
-
 	counter = 0
-	current_byte_num = int(byte_recieved[1])
-	temp_file = open(File_name[0]+".temp."+"txt", 'a+')
-	with open(FILENAME, 'wb') as file: 
+	current_byte_num = int(byte_recieved_split[1])
+	temp_file = open(File_name[0]+".temp."+"txt", 'wb')
+
+	with open(FILENAME, 'a+') as file: 
 		while True and counter<9:
-			print "in a damn loop"
 			data_recieved = clientSocket.recv(1024)
 			if not data_recieved:
 				break
@@ -194,14 +197,13 @@ def resume():
 			current_byte_num +=len(data_recieved)
 			counter+=1
 
-	print "++++++++++",current_byte_num
-
-
-
-
-
+	print current_byte_num
 	print request_2
 	print header2
+
+	current_byte_num = "Byte-recieved: " + str(current_byte_num)
+	NL = '\r\n'
+	temp_file.write(ETag + NL + Content_length + NL + current_byte_num + NL + Date_Modified)
 	clientSocket.close()
 
 
